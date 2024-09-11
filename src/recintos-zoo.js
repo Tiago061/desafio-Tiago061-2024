08afc4a19e3dc65f4de3cba606277fb3984b24a9
Tiago061 <Thenriqueleite277@gmail.com>
2024-09-10 21:47:35 -0300

    1 Falha, 5 Acertos

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,92 +1,75 @@
 class RecintosZoo {
-    constructor(){
-        this.recintos = [
-        { numero: 1, bioma: 'savana', tamanhoTotal: 10, animaisExistentes: [{ especie: 'MACACO', quantidade: 3 }] },
-        { numero: 2, bioma: 'floresta', tamanhoTotal: 5, animaisExistentes: [] },
-        { numero: 3, bioma: 'savana e rio', tamanhoTotal: 7, animaisExistentes: [{ especie: 'GAZELA', quantidade: 1 }] },
-        { numero: 4, bioma: 'rio', tamanhoTotal: 8, animaisExistentes: [] },
-        { numero: 5, bioma: 'savana', tamanhoTotal: 9, animaisExistentes: [{ especie: 'LEAO', quantidade: 1 }]}
-
-        ];
-    
-
-
-    this.tiposAnimais = [
-        { especie: 'LEAO', tamanho: 3, bioma: ['savana'], carnivoro: true },
-        { especie: 'LEOPARDO', tamanho: 2, bioma: ['savana'], carnivoro: true },
-        { especie: 'CROCODILO', tamanho: 3, bioma: ['rio'], carnivoro: true },
-        { especie: 'MACACO', tamanho: 1, bioma: ['savana', 'floresta'], carnivoro: false },
-        { especie: 'GAZELA', tamanho: 2, bioma: ['savana'], carnivoro: false },
-        { especie: 'HIPOPOTAMO', tamanho: 4, bioma: ['savana', 'rio'], carnivoro: false }
-    ];
-}
-
-    getAnimalInfo(animal){
-        return this.tiposAnimais.find(tipo => tipo.especie === animal);
+    constructor() {
+      this.recintos = [
+        { numero: 1, bioma: 'savana', tamanhoTotal: 10, animais: ['MACACO', 'MACACO', 'MACACO'] },
+        { numero: 2, bioma: 'floresta', tamanhoTotal: 5, animais: [] },
+        { numero: 3, bioma: 'savana e rio', tamanhoTotal: 7, animais: ['GAZELA'] },
+        { numero: 4, bioma: 'rio', tamanhoTotal: 8, animais: [] },
+        { numero: 5, bioma: 'savana', tamanhoTotal: 9, animais: ['LEAO'] }
+      ];
+  
+      this.animaisInfo = {
+        'LEAO': { tamanho: 3, biomas: ['savana'], carnivoro: true },
+        'LEOPARDO': { tamanho: 2, biomas: ['savana'], carnivoro: true },
+        'CROCODILO': { tamanho: 3, biomas: ['rio'], carnivoro: true },
+        'MACACO': { tamanho: 1, biomas: ['savana', 'floresta'], carnivoro: false },
+        'GAZELA': { tamanho: 2, biomas: ['savana'], carnivoro: false },
+        'HIPOPOTAMO': { tamanho: 4, biomas: ['savana', 'rio'], carnivoro: false }
+      };
     }
-
+  
     analisaRecintos(animal, quantidade) {
-        const resultado = {erro: null, recintosViáveis: [] };
-
-        const infoAnimal = this.getAnimalInfo(animal);
-        if(!infoAnimal){
-            resultado.erro = "Animal inválido";
-            return resultado;
+      if (!this.animaisInfo[animal]) {
+        return { erro: "Animal inválido" };
+      }
+  
+      if (quantidade <= 0) {
+        return { erro: "Quantidade inválida" };
+      }
+  
+      const animalInfo = this.animaisInfo[animal];
+      const recintosViaveis = [];
+  
+      for (let recinto of this.recintos) {
+        
+        if (!animalInfo.biomas.includes(recinto.bioma) && !(animalInfo.biomas.includes('savana') && recinto.bioma === 'savana e rio')) {
+          continue;
         }
-
-        if(quantidade <= 0){
-            resultado.erro = "Quantidade inválida";
-            return resultado;
+  
+        
+        const carnivorosExistentes = recinto.animais.filter(a => this.animaisInfo[a].carnivoro && a !== animal);
+        if (carnivorosExistentes.length > 0 && animalInfo.carnivoro) {
+          continue;
         }
-        for(const recinto of this.recintos) {
-            let espacoUsado = recinto.animaisExistentes.reduce((acc, a) => {
-                const info = this.getAnimalInfo(a.especie);
-                return acc + (a.quantidade * info.tamanho);
-            }, 0);
-            
-            if(recinto.animaisExistentes.length > 0){
-                const existente = recinto.animaisExistentes[0];
-                const infoExistente = this.getAnimalInfo(existente.especie);
+  
+        
+        const espacoOcupado = recinto.animais.reduce((acc, a) => acc + this.animaisInfo[a].tamanho, 0);
+        const espacoTotal = recinto.tamanhoTotal;
+        let espacoNecessario = animalInfo.tamanho * quantidade;
 
-                if(infoAnimal.carnivoro && (infoAnimal.especie !== infoExistente.especie)){
-                    continue;
-                }
-                
-                if(infoExistente.especie === 'HIPOPOTAMO' && recinto.bioma !== 'savana e rio'){
-                    continue;
-                }
-
-                if (infoExistente.especie === 'MACACO' && recinto.animaisExistentes.length === 1 && quantidade === 1) {
-                    continue;
-                }
-
-                if (infoAnimal.especie !== infoExistente.especie) {
-                    espacoUsado += 1;
-                }
+        if (recinto.animais.length > 0 && !recinto.animais.every(a => a === animal)) {
+            espacoNecessario += 1;
+          }
+  
+        if (espacoTotal - espacoOcupado >= espacoNecessario) {
+          recintosViaveis.push({
+            numero: recinto.numero,
+            espacoLivre: espacoTotal - (espacoOcupado + espacoNecessario),
+            espacoTotal
+          });
         }
-        if(!infoAnimal.bioma.includes(recinto.bioma)){
-            continue;
-        }
-
-        const espacoNecessario = infoAnimal.tamanho * quantidade;
-        const espacoLivre = recinto.tamanhoTotal - espacoUsado;
-        if(espacoLivre >= espacoNecessario){
-            resultado.recintosViáveis.push('Recinto ${recinto.numero} (espaço livre: ${espacoLivre} total: ${recinto.tamanhoTotal})');
-        }
+      }
+  
+      if (recintosViaveis.length === 0) {
+        return { erro: "Não há recinto viável" };
+      }
+  
+      return {
+        recintosViaveis: recintosViaveis.sort((a, b) => a.numero - b.numero).map(recinto =>
+          `Recinto ${recinto.numero} (espaço livre: ${recinto.espacoLivre} total: ${recinto.espacoTotal})`
+        )
+      };
     }
-    if(resultado.recintosViáveis.length === 0){
-        resultado.erro = "Não há recinto viável";
-    }
-
-    resultado.recintosViáveis.sort((a, b) => {
-        const numeroA = parseInt(a.match(/recinto (\d+)/)[1]);
-        const numeroB = parseInt(b.match(/recinto (\d+)/)[1]);
-        return numeroA - numeroB;
-    })
-
-    return resultado;
-}
-
-}
-
-export { RecintosZoo as RecintosZoo };
+  }
+  
+  export { RecintosZoo as RecintosZoo };
